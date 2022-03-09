a49993aa6d94ab9a51dcc46bff986bb9e63fdffc
safar1212 <mm9240584@gmail.com>
2022-03-09 14:40:05 +0500

    resolved the linters errors. Going to deploy the page

---
+++ index.js
@@ -0,0 +1,11 @@
+// header-section
+
+// const burgerIcon1 = document.querySelector('#burgerIcon');
+
+
+
+// burgerIcon1.addEventListener('click', () => {
+//     document.getElementById('cover').style.cssText = 'width: 100%; height: 2000px;';
+//     document.getElementById('cross1').style.cssText = 'display:block; z-index: 2';
+//     document.getElementById('menu').style.cssText = 'display: flex; z-index: 2;';
+//   });
